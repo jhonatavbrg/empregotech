dfd01b048c79b29b1dd7fd9e962548fc84ba9bbc
Jhonata <jhonatavbrg@gmail.com>
2022-11-30 04:19:46 -0300

    refactor: transform then function in async/await

--- js-challenge/assets/js/main.js
+++ js-challenge/assets/js/main.js
@@ -1,18 +1,12 @@
 import { fetchApi } from './pokeApi.js';
 
 const formatPokemonId = (id) => {
-    if(typeof(id) !== 'number') return `#000`;
+    if(isNaN(id)) return `#000`;
     if(id.toString().length === 1) return `#00${ id }`;
     else if(id.toString().length === 2) return `#0${ id }`;
 
     return `#${ id }`;
 };
-
-const capitalizePokemonName = (pokeName) => {
-    const firstLetterUpperCase = pokeName.charAt(0).toUpperCase();
-    const remainingLetters = pokeName.slice(1);
-    return firstLetterUpperCase + remainingLetters;
-}
 
 const createPokemonCard = (pokemon) => {
     const { name, id, types, image, backgroundColor } = pokemon;
@@ -20,23 +14,26 @@ const createPokemonCard = (pokemon) => {
     return `
             <li class="pokemon ${ backgroundColor }">
                 <span class="number">${ formatPokemonId(id) }</span>
-                <span class="name">${ capitalizePokemonName(name) }</span>
+                <span class="name">${ name }</span>
 
                 <div class="detail">
                     <ol class="types">
-                        ${ types.map(type => `<li>${ type }</li>`).join('') }
+                        ${ types.map(type => `<li class=${ type }>${ type }</li>`).join('') }
                     </ol>
-                    <img src=${ image } alt="${ capitalizePokemonName(name) }" />
+                    <img src=${ image } alt="${ name }" />
                 </div>
 
             </li>
         `;
 };
 
-fetchApi()
-    .then((pokemons = []) => {
-        const orderedList = document.querySelector('.pokemons');
+const handlePokemonLi = async (offset = 0, limit = 10) => {
+    const pokemons = await fetchApi(offset, limit) || [];
 
-        orderedList.innerHTML += pokemons.map(createPokemonCard).join('');
-    });
+    const orderedList = document.querySelector('.pokemons');
+
+    orderedList.innerHTML += pokemons.map(createPokemonCard).join('');
+};
+
+handlePokemonLi();
 
