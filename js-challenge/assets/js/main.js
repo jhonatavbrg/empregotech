888ca1ae3cbc85436e03150b1185590214f23394
Jhonata <jhonatavbrg@gmail.com>
2022-12-01 02:52:34 -0300

    feat: add pagination up to 151 pokemons

--- js-challenge/assets/js/main.js
+++ js-challenge/assets/js/main.js
@@ -27,13 +27,34 @@ const createPokemonCard = (pokemon) => {
         `;
 };
 
+
+const orderedList = document.querySelector('.pokemons');
+
 const handlePokemonLi = async (offset = 0, limit = 10) => {
     const pokemons = await fetchApi(offset, limit) || [];
-
-    const orderedList = document.querySelector('.pokemons');
 
     orderedList.innerHTML += pokemons.map(createPokemonCard).join('');
 };
 
 handlePokemonLi();
+
+const loadMoreBtn = document.querySelector('#loadMoreBtn');
+
+const loadMorePokemons = (event) => {
+    let limit = 0;
+    let elements = event.currentTarget;
+    elements.clicks = (elements.clicks || 1) + 1;
+    const { clicks } = elements;
+    
+    orderedList.childElementCount === 150 ? limit = 1 : limit = 5;
+
+    if(limit === 1) {
+        loadMoreBtn.setAttribute('disabled', '');
+        loadMoreBtn.removeEventListener('click', loadMorePokemons);
+    } 
+
+    handlePokemonLi((5 * clicks), limit);
+};
+
+loadMoreBtn.addEventListener('click', loadMorePokemons);
 
